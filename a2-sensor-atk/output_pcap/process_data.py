0d94b6c972b7717a3b0ea11080dbe12a997ee231
OMGhozlan <osama.ghozlan@gmail.com>
2021-05-28 17:08:56 +0200

    Combined csv files

--- a2-sensor-atk/output_pcap/process_data.py
+++ a2-sensor-atk/output_pcap/process_data.py
@@ -1,8 +1,12 @@
 import pandas as pd
 
-columns = ['Session Key', 'Protocol', 'Src IP', 'Src Port', 
-'Dst IP', 'Dst Port', 'Init Ts', 'Last Ts', 'Ts', 'Duration', 'Mac Addr', 'Type']
+columns = ['Protocol', 'Src IP', 'Src Port', 
+'Dst IP', 'Dst Port', 'Init Ts', 'Last Ts', 'Ts', 'Duration', 'Mac Addr', 'Label']
 
+df = []
 for i in range(1, 4):
-    df = pd.read_csv(f'a2_{i}_mod.csv')
-    df[columns].to_csv(f'a2_{i}_modf.csv')
+    df.append(pd.read_csv(f'a2_{i}_labeled.csv'))
+
+df = pd.concat(df)
+
+df[columns].to_csv(f'data.csv', index=False)
