b4db93cfb69191137fd20d5f5a2cb6ed516b269c
OMGhozlan <osama.ghozlan@gmail.com>
2021-05-31 00:32:40 +0200

    Added features variant

--- a2-sensor-atk/output_pcap/process_data.py
+++ a2-sensor-atk/output_pcap/process_data.py
@@ -1,12 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
 import pandas as pd
 
-columns = ['Protocol', 'Src IP', 'Src Port', 
+
+columns = ['Session Key', 'Protocol', 'Src IP', 'Src Port', 
 'Dst IP', 'Dst Port', 'Init Ts', 'Last Ts', 'Ts', 'Duration', 'Mac Addr', 'Label']
 
-df = []
-for i in range(1, 4):
-    df.append(pd.read_csv(f'a2_{i}_labeled.csv'))
+def cat_files():
+    df = []
+    for i in range(1, 4):
+        df.append(pd.read_csv(f'a2_{i}_labeled.csv'))
 
-df = pd.concat(df)
+    df = pd.concat(df)
+    return df
 
-df[columns].to_csv(f'data.csv', index=False)
+
+if __name__ == '__main__':
+    df = cat_files()
+    if len(sys.argv) == 2:
+        df[columns[1:]].to_csv(f'data.csv', index=False)
+    else:
+        df.drop(columns=columns[:-1], inplace=True)
+        df.fillna(0, inplace=True)
+        df.to_csv(f'data_l.csv', index=False)
+    print("[*] Done!")
