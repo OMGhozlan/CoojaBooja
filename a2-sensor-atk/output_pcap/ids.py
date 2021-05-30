04acd60b894b00fb563d9c4719cb3338cf37fc79
OMGhozlan <osama.ghozlan@gmail.com>
2021-05-31 01:44:15 +0200

    Adjusted preprocessing

--- a2-sensor-atk/output_pcap/ids.py
+++ a2-sensor-atk/output_pcap/ids.py
@@ -1,8 +1,6 @@
+import sys
 import pandas as pd
 import numpy as np
-import netaddr
-import re
-from datetime import datetime
 from sklearn.naive_bayes import ComplementNB, GaussianNB, MultinomialNB, BernoulliNB, CategoricalNB
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.model_selection import KFold, cross_val_score, cross_validate
@@ -11,72 +9,31 @@ from sklearn.metrics import (classification_report, confusion_matrix,
                              precision_recall_fscore_support)
 
 
-def timestamp(dt):
-    zero = datetime(1900, 1, 1, 0, 0, 0, 0)
-    dt = datetime.strptime(dt,'%M:%S.%f')
-    return (dt - zero).total_seconds() * 1000.0
+if __name__ == '__main__':
 
+       kf = KFold(n_splits=10, shuffle=True)
+       
+       file_ = None
+       if len(sys.argv) == 1:
+              while not file_:
+                     file_ = input("Enter file name:")
+       else:
+              file_ = sys.argv[1]
 
-x_columns = ['icmpv6', 'udp', 'wpan', 'src_mac', 'src_ip', 'src_port', 'dst_mac', 'dst_ip', 'dst_port',
-       'init_ts', 'last_ts', 'ts', 'duration']
+       df = pd.read_csv(file_)
 
-# Load data
-df = pd.read_csv("data.csv")
+       features = list(df.columns)
+       features.remove('label')
 
-dst_mac = []
-src_mac = []
-for i in range(len(df)):
-       macs = df['Mac Addr'][i]
-       if macs == 'set()':
-              m = [[0, 0], [0, 0]]
-       else:       
-              m = re.findall('(([0-9A-Fa-f]{2}[:-]){7}[0-9A-Fa-f]{2})', df['Mac Addr'][i])
-       dst_mac.append(int(netaddr.EUI(m[0][0])))
-       src_mac.append(int(netaddr.EUI(m[1][0])))
+       X = df[features]
+       y = df['label']
 
-df = pd.DataFrame({'src_mac': src_mac, 'dst_mac': dst_mac}).join(df)
-del dst_mac, src_mac
+       clfs = [AdaBoostClassifier(n_estimators=100), RandomForestClassifier(n_estimators=100), GaussianNB(), MultinomialNB(), BernoulliNB()] #, ComplementNB()]
 
-# Perform one-hot encoding on protocols used
-df = pd.get_dummies(df['Protocol']).join(df)
+       cv_results = []
 
-# Drop the 'protocol' column
-df.drop('Protocol', axis=1, inplace=True)
-
-
-# Drop the 'mac_addr' column
-df.drop('Mac Addr', axis=1, inplace=True)
-
-# Turn label into numerical (0: Normal, 1: Malicious)
-df['Label'] = (df['Label'] == 'Malicious').astype(int)
-
-# Renaming columns (ease of use)
-df.columns = [col.lower().replace(" ", "_") for col in df.columns]
-
-# Convert IP address into int to be fed into classifier
-df['src_ip'] = df['src_ip'].apply(lambda x: netaddr.IPAddress(x) if x != '0' else 0)
-df['dst_ip'] = df['dst_ip'].apply(lambda x: netaddr.IPAddress(x) if x != '0' else 0)
-
-# Convert times into milliseconds to be fed into classifier
-df['init_ts'] = df['init_ts'].apply(lambda x: timestamp(x))
-df['last_ts'] = df['last_ts'].apply(lambda x: timestamp(x))
-
-
-df = df[['icmpv6', 'udp', 'wpan', 'src_mac', 'src_ip', 'src_port', 'dst_mac', 'dst_ip', 'dst_port',
-       'init_ts', 'last_ts', 'ts', 'duration', 'label']]
-df.to_csv('data_processed.csv', index=False)
-
-X = df[x_columns]
-y = df['label']
-
-kf = KFold(n_splits=10, shuffle=True)
-
-clfs = [AdaBoostClassifier(n_estimators=100), RandomForestClassifier(n_estimators=100), GaussianNB(), MultinomialNB(), BernoulliNB()] #, ComplementNB()]
-
-cv_results = []
-
-for clf in clfs:
-    print(clf)
-    cv_results.append(pd.DataFrame(cross_validate(clf, X, y, cv=kf, scoring=('accuracy', 'f1', 'precision', 'recall'))))
-    
-print(cv_results)
+       for clf in clfs:
+              print(clf)
+              cv_results.append(pd.DataFrame(cross_validate(clf, X, y, cv=kf, scoring=('accuracy', 'f1', 'precision', 'recall'))))
+       
+       print(cv_results)
--- a2-sensor-atk/output_pcap/process_data.py
+++ a2-sensor-atk/output_pcap/process_data.py
@@ -3,10 +3,71 @@
 import sys
 import numpy as np
 import pandas as pd
+import netaddr
+import re
+from datetime import datetime
 
 
-columns = ['Session Key', 'Protocol', 'Src IP', 'Src Port', 
-'Dst IP', 'Dst Port', 'Init Ts', 'Last Ts', 'Ts', 'Duration', 'Mac Addr', 'Label']
+columns = ['session_key', 'protocol', 'src_ip', 'src_port','dst_ip', 'dst_port', 
+'init_ts', 'last_ts', 'ts', 'duration', 'mac_addr','label']
+
+
+x_columns = ['icmpv6', 'udp', 'wpan', 'src_mac', 'src_ip', 'src_port', 'dst_mac', 'dst_ip', 'dst_port',
+       'init_ts', 'last_ts', 'ts', 'duration']
+
+def timestamp(dt):
+    zero = datetime(1900, 1, 1, 0, 0, 0, 0)
+    dt = datetime.strptime(dt,'%M:%S.%f')
+    return (dt - zero).total_seconds() * 1000.0
+
+
+def process_other_feats(file="data.csv", to_csv=True):
+       # Load data
+       df = pd.read_csv(file)
+
+       dst_mac = []
+       src_mac = []
+       for i in range(len(df)):
+              macs = df['mac_addr'][i]
+              if macs == 'set()':
+                     m = [[0, 0], [0, 0]]
+              else:       
+                     m = re.findall('(([0-9A-Fa-f]{2}[:-]){7}[0-9A-Fa-f]{2})', df['mac_addr'][i])
+              dst_mac.append(int(netaddr.EUI(m[0][0])))
+              src_mac.append(int(netaddr.EUI(m[1][0])))
+
+       df = pd.DataFrame({'src_mac': src_mac, 'dst_mac': dst_mac}).join(df)
+       del dst_mac, src_mac
+
+       df['protocol'] = df['protocol'].apply(lambda x: x.lower())
+
+       # Perform one-hot encoding on protocols used
+       df = pd.get_dummies(df['protocol']).join(df)
+       print(df.head())
+
+       # Drop the 'protocol' column
+       df.drop('protocol', axis=1, inplace=True)
+
+       # Drop the 'mac_addr' column
+       df.drop('mac_addr', axis=1, inplace=True)
+
+       # Turn label into numerical (0: Normal, 1: Malicious)
+       df['label'] = (df['label'] == 'Malicious').astype(int)
+
+       # Convert IP address into int to be fed into classifier
+       df['src_ip'] = df['src_ip'].apply(lambda x: int(netaddr.IPAddress(x)) if x != '0' else 0)
+       df['dst_ip'] = df['dst_ip'].apply(lambda x: int(netaddr.IPAddress(x)) if x != '0' else 0)
+
+       # Convert times into milliseconds to be fed into classifier
+       df['init_ts'] = df['init_ts'].apply(lambda x: timestamp(x))
+       df['last_ts'] = df['last_ts'].apply(lambda x: timestamp(x))
+
+
+       df = df[['icmpv6', 'udp', 'wpan', 'src_mac', 'src_ip', 'src_port', 'dst_mac', 'dst_ip', 'dst_port',
+              'init_ts', 'last_ts', 'ts', 'duration', 'label']]
+       if to_csv:
+              df.to_csv('data_processed.csv', index=False)
+       return df
 
 def cat_files():
     df = []
@@ -14,6 +75,7 @@ def cat_files():
         df.append(pd.read_csv(f'a2_{i}_labeled.csv'))
 
     df = pd.concat(df)
+    df.columns = [col.lower().replace(" ", "_") for col in df.columns]
     return df
 
 
